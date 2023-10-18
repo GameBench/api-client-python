6aca38ec8157c08e10b4e6c13dab3ec92c53f945
Emilia Slade <emilia@gamebench.net>
2023-10-18 15:02:13 +0100

    Require session ID arg

--- export-example.py
+++ export-example.py
@@ -1,9 +1,16 @@
 from client import ApiClient
 import os
+import sys
+
+if len(sys.argv) != 2:
+    print('Please provide session ID as first argument')
+    sys.exit(1)
+
+session_id = sys.argv[1]
 
 client = ApiClient(os.environ.get('GB_API_BASE_URL'), os.environ.get('GB_COMPANY_ID'), (os.environ.get('GB_API_EMAIL'), os.environ.get('GB_API_TOKEN')))
 
-session = client.getSession('fb16412b-a174-4c9a-acb3-8e5e728ed3e3')
-client.exportSession('fb16412b-a174-4c9a-acb3-8e5e728ed3e3', 'session.zip')
+session = client.getSession(session_id)
+client.exportSession(session_id, 'session_%s.zip' % session_id)
 
-client.downloadSessionCsv('fb16412b-a174-4c9a-acb3-8e5e728ed3e3', 'session.csv')
+client.downloadSessionCsv(session_id, 'session_%s.csv' % session_id)
